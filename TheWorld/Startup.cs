369438bd2cace060a36eee958cca71aed024d164
Samuel Thompson <nevdka@gmail.com>
2017-03-11 19:13:27 +1100

    End of part 5: MVC 6

--- TheWorld/Startup.cs
+++ TheWorld/Startup.cs
@@ -5,24 +5,61 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using TheWorld.Services;
 
 namespace TheWorld
 {
     public class Startup
     {
+        private IHostingEnvironment _env;
+        private IConfigurationRoot _config;
+
+        public Startup(IHostingEnvironment env )
+        {
+            _env = env;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath( _env.ContentRootPath )
+                .AddJsonFile( "config.json" )
+                .AddEnvironmentVariables();
+
+            _config = builder.Build();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton( _config );
+
+            if(_env.IsDevelopment() || _env.IsEnvironment("Testing"))
+            {
+                services.AddScoped<IMailService, DebugMailService>();
+            }
+
+            services.AddMvc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            app.UseDefaultFiles();
+            if(env.IsDevelopment())
+            {
+            app.UseDeveloperExceptionPage();
+            }
             app.UseStaticFiles();
+
+            app.UseMvc(config =>
+            {
+                config.MapRoute(
+                    name: "Default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "App", action = "Index" }
+                    );
+            });
         }
     }
 }

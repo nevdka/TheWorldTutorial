37a6e5ab6e2aeef2bc330abedb9ad11647937252
Samuel Thompson <nevdka@gmail.com>
2017-03-12 14:11:02 +1100

    Added bootstrap.

--- TheWorld/Controllers/Web/AppController.cs
+++ TheWorld/Controllers/Web/AppController.cs
@@ -33,10 +33,7 @@ namespace TheWorld.wwwroot.css.Controllers.Web
         [HttpPost]
         public IActionResult Contact( ContactViewModel model )
         {
-            if ( model.Email.Contains( "aol.com" ) )
-            {
-                ModelState.AddModelError( "", "We don't support AOL addresses" );
-            }
+
             if ( ModelState.IsValid )
             {
                 _mailService.SendMail( _config["MailSettings:ToAddress"], model.Email, "From TheWorld", model.Message );
